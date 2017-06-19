bb234db2ecfe6174cd044bfaa1381b7cfe36cd41
Robert Seifert <rseifert.phone@gmail.com>
2017-06-19 02:45:50 -0400

    Added recipes for each subtype

--- src/main/java/com/builtbroken/woodenshears/WoodenShears.java
+++ src/main/java/com/builtbroken/woodenshears/WoodenShears.java
@@ -1,8 +1,9 @@
 package com.builtbroken.woodenshears;
 
 import com.builtbroken.woodenshears.content.ItemWoodenShear;
+import com.builtbroken.woodenshears.content.WoodTypes;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
@@ -11,10 +12,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.util.List;
 
 @Mod(modid = WoodenShears.DOMAIN, name = "Wooden Shears", version = "@MAJOR@.@MINOR@.@REVIS@.@BUILD@")
 public class WoodenShears
@@ -57,7 +61,26 @@ public class WoodenShears
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-        GameRegistry.addShapedRecipe(new ItemStack(itemShears), "w w", " t ", "s s", 'w', new ItemStack(Blocks.PLANKS), 't', Blocks.SAPLING, 's', Items.STICK);
+        for (WoodTypes type : WoodTypes.values())
+        {
+            type.registerRecipe();
+        }
+        //Support for ore dictionary planks that are not natively supported with custom textures
+        List<ItemStack> items = OreDictionary.getOres("plankWood");
+        for (ItemStack stack : items)
+        {
+            if (stack != null && stack.getItem() != Item.getItemFromBlock(Blocks.PLANKS))
+            {
+                //TODO ensure type is not already supported
+                GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(WoodenShears.itemShears),
+                        "w w",
+                        " t ",
+                        "s s",
+                        'w', stack,
+                        't', Blocks.SAPLING,
+                        's', "stickWood"));
+            }
+        }
         proxy.postInit();
         config.save();
     }
--- src/main/java/com/builtbroken/woodenshears/content/WoodTypes.java
+++ src/main/java/com/builtbroken/woodenshears/content/WoodTypes.java
@@ -1,7 +1,13 @@
 package com.builtbroken.woodenshears.content;
 
 import com.builtbroken.woodenshears.WoodenShears;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -9,25 +15,33 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
  */
 public enum WoodTypes
 {
-    OAK("oak", true),
-    ACACIA("acacia", true),
-    BIRCH("birch", true),
-    JUNGLE("jungle", true),
-    SPRUCE("spruce", true),
-    BIG_OAK("big_oak", true),
-    CHARRED("charred", true);
+    OAK("oak", "minecraft:planks", BlockPlanks.EnumType.OAK.ordinal()),
+    ACACIA("acacia", "minecraft:planks", BlockPlanks.EnumType.ACACIA.ordinal()),
+    BIRCH("birch", "minecraft:planks", BlockPlanks.EnumType.BIRCH.ordinal()),
+    JUNGLE("jungle", "minecraft:planks", BlockPlanks.EnumType.JUNGLE.ordinal()),
+    SPRUCE("spruce", "minecraft:planks", BlockPlanks.EnumType.SPRUCE.ordinal()),
+    BIG_OAK("big_oak", "minecraft:planks", BlockPlanks.EnumType.DARK_OAK.ordinal()),
+    CHARRED("charred");
+    //TODO add support for other mods
 
     /** Name to use for textures and unlocalizations */
     public final String name;
-    /** Is the item enabled, normally only used when adding modding materials */
-    public boolean enabled;
+
+    public String blockID;
+    public int blockMeta;
 
     private ModelResourceLocation modelResourceLocation;
 
-    WoodTypes(String name, boolean enabled)
+    WoodTypes(String name, String blockID, int meta)
+    {
+        this(name);
+        this.blockID = blockID;
+        this.blockMeta = meta;
+    }
+
+    WoodTypes(String name)
     {
         this.name = name;
-        this.enabled = enabled;
     }
 
     /** Model for this type */
@@ -38,5 +52,25 @@ public enum WoodTypes
             modelResourceLocation = new ModelResourceLocation(WoodenShears.itemShears.getRegistryName() + "_" + name, "inventory");
         }
         return modelResourceLocation;
+    }
+
+    public void registerRecipe()
+    {
+        if(blockID != null)
+        {
+            Block block = BlockPlanks.getBlockFromName(blockID);
+            if(block != null)
+            {
+                ItemStack stack = new ItemStack(WoodenShears.itemShears);
+                WoodenShears.itemShears.setType(stack, this);
+                GameRegistry.addRecipe(new ShapedOreRecipe(stack,
+                        "w w",
+                        " t ",
+                        "s s",
+                        'w', new ItemStack(block, 1, blockMeta),
+                        't', Blocks.SAPLING, 's', "stickWood"));
+            }
+        }
+
     }
 }
