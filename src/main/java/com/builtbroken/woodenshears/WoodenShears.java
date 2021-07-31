af0892b5a0978768b2203f938ed68954ca4e8d23
Robin Seifert <rseifert.phone@gmail.com>
2021-07-31 17:37:28 -0400

    Changed dispenser logic to register in FmlCommonSetupEvent

--- src/main/java/com/builtbroken/woodenshears/WoodenShears.java
+++ src/main/java/com/builtbroken/woodenshears/WoodenShears.java
@@ -3,8 +3,8 @@ package com.builtbroken.woodenshears;
 import com.builtbroken.woodenshears.content.WoodTypes;
 import com.builtbroken.woodenshears.content.WoodenShearItem;
 import net.minecraft.core.dispenser.ShearsDispenseItemBehavior;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Arrays;
 
@@ -29,10 +31,17 @@ public class WoodenShears
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-        Arrays.stream(WoodTypes.values()).forEach(type -> {
-            ShearsItem shears = new WoodenShearItem(type);
-            event.getRegistry().register(shears);
-            DispenserBlock.registerBehavior(shears, new ShearsDispenseItemBehavior());
-        });
+        Arrays.stream(WoodTypes.values()).forEach(type -> event.getRegistry().register(new WoodenShearItem(type)));
+    }
+
+    @SubscribeEvent
+    public static void commonSetup(FMLCommonSetupEvent event)
+    {
+        //Common is invoked off main thread, queue to main thread
+        event.enqueueWork(() -> Arrays.stream(WoodTypes.values()).forEach(type -> {
+            //Look up item, other mods may replace our items for things such as progression
+            final Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(DOMAIN, type.itemName));
+            DispenserBlock.registerBehavior(item, new ShearsDispenseItemBehavior());
+        }));
     }
 }
